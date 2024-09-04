66a477731087c254db8cf53b9d21d3fad3c54127
전 유빈 <ubing0101@reflexion.co.kr>
2024-09-04 17:27:44 +0900

    Add Netlify functions

---
+++ functions/movie.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+exports.handler = async function (event){
+    console.log(event);
+    const payload = JSON.parse(event.body);
+    const { title, type, year, page, id } = payload;
+
+    const OMDB_API_KEY = '7035c60c';
+    const url = `https://www.omdbapi.com?apikey=${OMDB_API_KEY}` + (id ? `&i=${id}` : `&s=${title}&type=${type}&y=${year}&page=${page}`);
+
+    try {
+        const { data } = await axios.get(url);
+        if (data.Error) {
+            return {
+                statusCode: 400,
+                body: data.Error
+            }
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        }
+    } catch (error) {
+        return {
+            statusCode: error.response.status,
+            body: error.message
+        }
+    }
+}
--- src/store/movie.js
+++ src/store/movie.js
@@ -4,29 +4,25 @@ import _uniqBy from 'lodash/uniqBy'
 const _defaultMessage = 'Search for the movie title!'
 
 export default {
-  // module!
+  // 현재 파일(movie.js)을 Store 모듈로 활용하려면 다음 옵션이 필요합니다.
   namespaced: true,
 
-  // data!
+  // Vue.js data 옵션과 유사합니다.
+  // 상태(State)는 함수로 만들어서 객체 데이터를 반환해야 가변 이슈(데이터 불변성)가 발생하지 않습니다!
   state: () => ({
     movies: [],
     message: _defaultMessage,
     loading: false,
-    thisMovie: {}
+    theMovie: {}
   }),
 
-  // computed!
-  getters: {
-    movieIds(state) {
-      return state.movies.map(m => m.imdbID)
-    }
-  },
+  // Vue.js computed 옵션과 유사합니다.
+  getters: {},
 
-  // methods!
-  // 변이
+  // Vue.js methods 옵션과 유사합니다.
+  // 상태(State)는 변이(Mutations)를 통해서만 값을 바꿀 수 있습니다.
   mutations: {
     updateState(state, payload) {
-      // ['movies', 'message', 'loading']
       Object.keys(payload).forEach(key => {
         state[key] = payload[key]
       })
@@ -37,12 +33,14 @@ export default {
       state.loading = false
     }
   },
-  
-  // 비동기
+
+  // Vue.js methods 옵션과 유사합니다.
+  // 변이(Mutations)가 아닌 나머지 모든 로직을 관리합니다.
+  // 비동기로 동작합니다.
   actions: {
     async searchMovies({ state, commit }, payload) {
-
-      if(state.loading) return
+      // const { title, type, number, year } = payload
+      if (state.loading) return
 
       commit('updateState', {
         message: '',
@@ -54,18 +52,17 @@ export default {
           ...payload,
           page: 1
         })
-  
         const { Search, totalResults } = res.data
         commit('updateState', {
           movies: _uniqBy(Search, 'imdbID')
         })
-  
+
+        // ceil = 올림!
         const total = parseInt(totalResults, 10)
-        const pageLength = Math.ceil(total / 10)
-  
+        const pageLength = Math.ceil(total / 10) // 총 페이지의 길이
+
         // 추가 요청!
-        if(pageLength > 1) {
-  
+        if (pageLength > 1) {
           for (let page = 2; page <= pageLength; page += 1) {
             if (page > (payload.number / 10)) break
             const res = await _fetchMovie({
@@ -81,11 +78,10 @@ export default {
             })
           }
         }
-
-      } catch (message) {
+      } catch ({ message }) {
         commit('updateState', {
-          moives: [],
-          message: message
+          movies: [],
+          message
         })
       } finally {
         commit('updateState', {
@@ -94,11 +90,12 @@ export default {
       }
     },
     async searchMovieWithId({ state, commit }, payload) {
-      if(state.loading) return
+      // const { id } = payload
+      if (state.loading) return
 
       commit('updateState', {
         theMovie: {},
-        loading: true
+        loading: true,
       })
 
       try {
@@ -119,21 +116,7 @@ export default {
   }
 }
 
-function _fetchMovie(payload) {
-  const { title, type, year, page, id } = payload
-  const OMDB_API_KEY = '7035c60c'
-  const url = `https://www.omdbapi.com?apikey=${OMDB_API_KEY}` + (id ? `&i=${id}` : `&s=${title}&type=${type}&y=${year}&page=${page}`)
-
-  return new Promise((resolve, reject) => {
-    axios.get(url)
-    .then((res) => {
-      if (res.data.Error) {
-        reject(res.data.Error)
-      }
-      resolve(res)
-    })
-    .catch(err => {
-      reject(err.message)
-    })
-  })
+// eslint-disable-next-line
+async function _fetchMovie(payload) {
+  return await axios.post('/.netlify/functions/movie', payload)
 }
