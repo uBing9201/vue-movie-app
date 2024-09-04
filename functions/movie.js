0fe0dc4af6fa420e8ccebd417326ba7a8507d080
전 유빈 <ubing0101@reflexion.co.kr>
2024-09-04 17:49:27 +0900

    Add Netlify functions

--- functions/movie.js
+++ functions/movie.js
@@ -1,12 +1,17 @@
-const axios = require('axios');
 
-exports.handler = async function (event){
-    console.log(event);
+const axios = require('axios');
+const { OMDB_API_KEY } = process.env;
+
+exports.handler = async function (event) {
     const payload = JSON.parse(event.body);
     const { title, type, year, page, id } = payload;
 
-    const OMDB_API_KEY = '7035c60c';
-    const url = `https://www.omdbapi.com?apikey=${OMDB_API_KEY}` + (id ? `&i=${id}` : `&s=${title}&type=${type}&y=${year}&page=${page}`);
+    console.log('OMDB_API_KEY: ', OMDB_API_KEY);
+    console.log('params: ', payload);
+
+    const url = id
+            ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}&plot=full`
+            : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
 
     try {
         const { data } = await axios.get(url);
@@ -14,16 +19,16 @@ exports.handler = async function (event){
             return {
                 statusCode: 400,
                 body: data.Error
-            }
+            };
         }
         return {
             statusCode: 200,
             body: JSON.stringify(data)
-        }
+        };
     } catch (error) {
         return {
             statusCode: error.response.status,
             body: error.message
-        }
+        };
     }
 }
